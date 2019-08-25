a689c0d434da720b5d3bbf0206ce4bcdf95a7931
minnja6 <jasmine.minniweather@verizon.com>
2019-08-25 18:10:50 -0500

    updates
    updates to pugs

--- models/book.js
+++ models/book.js
@@ -1,64 +1,13 @@
+'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Book = sequelize.define('Book', {
-    title: {
-      type: DataTypes.STRING,
-      validate: {
-        notEmpty: {
-          msg: "Title is required"
-        }
-      }
-    },
-    author: {
-      type: DataTypes.STRING,
-      validate: {
-        notEmpty: {
-          msg: "Author is required"
-        }
-      }
-    },
+    title: DataTypes.STRING,
+    author: DataTypes.STRING,
     genre: DataTypes.STRING,
     year: DataTypes.INTEGER
   }, {});
   Book.associate = function(models) {
     // associations can be defined here
   };
-
   return Book;
 };
-
-
-
-
-
-
-
-// const Sequelize = require('sequelize');
-// const db = require('../config/database');
-
-// const Book = db.define('book', {
-//     title: {
-//       type: Sequelize.STRING,
-//       validate: {
-//         notEmpty: {
-//           msg: "Title is required"
-//         }
-//       }
-//     },
-//     author: {
-//       type: Sequelize.STRING,
-//       validate: {
-//         notEmpty: {
-//           msg: "Author is required"
-//         }
-//       }
-//     },
-//     genre: {
-//       type: Sequelize.STRING
-//     },
-//     year: {
-//       type: Sequelize.INTEGER
-//     }
-//   });
-
-// module.exports = Book;
-
